0483efca76ece08a4563e9e7039fa3195838980d
Jay <jdub.kim@gmail.com>
2019-08-07 17:16:23 +0900

    icon changed

--- src/AuthMain.js
+++ src/AuthMain.js
@@ -249,14 +249,14 @@ export default class AuthMain extends React.Component {
                         <Image
                             style={{
                                 tintColor: 'rgba(255, 255, 255, 0.8)',
-                                width: 31, height: 31,
+                                width: 48, height: 48,
                                 resizeMode: 'cover'
                             }}
                             source={PreloadImage.logo}
                         />
                         <Text style={{
                             // backgroundColor: 'green',
-                            marginLeft: 4,
+                            marginLeft: -4,
                             fontFamily: "FriendlySchoolmates-Regular",
                             color: 'rgba(255, 255, 255, 0.8)',
                             fontSize: 40,
