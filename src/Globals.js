4dd41552c75230a64a492d25bf86ad7f8a3a1e9e
Jay <jdub.kim@gmail.com>
2019-12-07 14:35:49 +0900

    topic notification

--- src/Globals.js
+++ src/Globals.js
@@ -21,7 +21,7 @@ export var Vars = {
 export const Cons = {
     version: '1.1.30',
     buildNumber: '93088300',
-    lastUpdatedDate: 'Nov 23, 2019 12:30',
+    lastUpdatedDate: 'Dec 05, 2019 12:57',
 
     // push notification type
     pushNotification: {
